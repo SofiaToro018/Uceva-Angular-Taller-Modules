af5d8deb672071065a6f57f4b2f37a6a568f36a5
Sofia Toro <sofiatoro038@gmail.com>
2026-03-05 19:08:48 -0500

    test: Ajuste de prueba

--- src/app/modules/movies/components/table-movies/table-movies.component.spec.ts
+++ src/app/modules/movies/components/table-movies/table-movies.component.spec.ts
@@ -3,7 +3,6 @@ import { TableMoviesComponent } from './table-movies.component';
 import { MOVIES } from '../../../../core/config/movies.config';
 import { By } from '@angular/platform-browser';
 import { BadgeComponent } from '../../../shared/components/badge/badge.component';
-import { CurrencyPipe } from '@angular/common';
 
 describe('TableMoviesComponent', () => {
   let component: TableMoviesComponent;
@@ -11,7 +10,7 @@ describe('TableMoviesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TableMoviesComponent, BadgeComponent, CurrencyPipe],
+      declarations: [TableMoviesComponent, BadgeComponent]
     })
     .compileComponents();
 
@@ -40,21 +39,20 @@ describe('TableMoviesComponent', () => {
     rows.forEach((row, index) => {
       const columns = row.queryAll(By.css('th, td'));
       const movie = component.movies[index];
-      const movieYear = new CurrencyPipe('en-US').transform(movie.year);
 
       expect(columns[0].nativeElement.textContent.trim()).toBe(String(movie.id));
       expect(columns[1].nativeElement.textContent.trim()).toBe(movie.title);
       expect(columns[2].nativeElement.textContent.trim()).toBe(movie.director);
-      expect(columns[3].nativeElement.textContent.trim()).toBe(movieYear);
+      expect(columns[3].nativeElement.textContent.trim()).toBe(String(movie.year));
       expect(columns[4].nativeElement.textContent.trim()).toBe(movie.genre);
     });
   });
   it('debería mapear cada género a su BadgeType correcto', () => {
     expect(component.genreMap['Drama']).toBe('primary');
     expect(component.genreMap['Crimen']).toBe('danger');
-    expect(component.genreMap['Ciencia Ficción']).toBe('warning');
-    expect(component.genreMap['Acción']).toBe('success');
-    expect(component.genreMap['Comedia']).toBe('info');
+    expect(component.genreMap['Ciencia Ficción']).toBe('info');
+    expect(component.genreMap['Acción']).toBe('warning');
+    expect(component.genreMap['Comedia']).toBe('success');
     expect(component.genreMap['Terror']).toBe('dark');
     expect(component.genreMap['Aventura']).toBe('secondary');
   });
